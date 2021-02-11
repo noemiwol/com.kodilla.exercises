9083ac532daba79ab70012339acbd7fdc800d4e7
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2021-02-11 21:20:23 +0100

    I created a class with a path to the file

---
+++ springIntegration/src/main/java/com/kodilla/springIntegration/configuration/FilePathsConstants.java
@@ -0,0 +1,7 @@
+package com.kodilla.springIntegration.configuration;
+
+public class FilePathsConstants {
+    public static final String pathInput ="springIntegration/data/input";
+    public static final String pathOutput = "springIntegration/data/output";
+
+}
