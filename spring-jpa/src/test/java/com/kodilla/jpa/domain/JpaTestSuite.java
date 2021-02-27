4a31ee1eb75beb656f47c9b2199d870b92d03d8d
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2021-02-27 15:00:37 +0100

    done

---
+++ spring-jpa/src/test/java/com/kodilla/jpa/domain/JpaTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.jpa.domain;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class JpaTestSuite {
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Test
+    void shouldPersistUser() {
+        //Given
+        EntityManager em = emf.createEntityManager();
+        User kodilla =
+                new User(null, "Kodilla",  "Wrocław");
+
+        //When
+        em.getTransaction().begin();
+        em.persist(kodilla);
+        em.flush();
+        em.getTransaction().commit();
+
+        //Then
+        Long key = kodilla.getId();
+        User readCustomer = em.find(User.class, key);
+        em.close();
+        assertThat(readCustomer.getName()).isEqualTo(kodilla.getName());
+    }
+}
